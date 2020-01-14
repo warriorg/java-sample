43d5ea61eef88f40ff59440afaf92cd2750df8e7
Warrior Gao <warrior.gao@gmail.com>
2020-01-14 08:38:02 +0800

    java jdbc study

--- spring-event/src/main/java/me/warriorg/spring/event/controller/FooController.java
+++ spring-event/src/main/java/me/warriorg/spring/event/controller/FooController.java
@@ -3,7 +3,6 @@ package me.warriorg.spring.event.controller;
 import me.warriorg.spring.event.FooEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
